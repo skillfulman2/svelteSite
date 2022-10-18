a6a3e3dcfe9a8e0d136a74b5755ffcd8a744add3
Ryan Remaly <remalyryan@gmail.com>
2022-10-18 14:38:14 -0400

    Adding production configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
+//import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
 	},
 
 	extensions: ['.svelte', '.md'],
