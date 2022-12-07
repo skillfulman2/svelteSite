2c97b09ca1efc6a8bfd35a7ec687b5b830efbca1
Ryan Remaly <remalyryan@gmail.com>
2022-12-06 21:34:46 -0500

    Adding logos and changing footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,115 +1,22 @@
 <script lang="ts">
-    import Grid from './Grid.svelte'
-  
-    const currentYear: number = new Date().getFullYear()
-  </script>
-  
-  
-  <div class="footer-wrapper">
-    <Grid inverted={true} />
-    <footer class="footer">
-      <div class="footer__links">
-        <h2 class="h4" id="footer-pages">Pages</h2>
-        <nav>
-          <ul aria-labelledby="footer-pages">
-            <li><a href="/">Home/About</a></li>
-            <li><a href="/blog">Blog</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </nav>
-      </div>
-      <div class="footer__links">
-        <h2 class="h4" id="footer-links">Links</h2>
-        <ul aria-labelledby="footer-links">
-          <li><a href="https://github.com/skillfulman2" rel="me">GitHub</a></li>
-          <li><a href="https://codepen.io/skillfulman2">CodePen</a></li>
-        </ul>
-      </div>
-  
-      <div class="fine-print">
-        <p>©1997–{ currentYear } Ryan Remaly</p>
-  
-        
-      </div>
-    </footer>
-  </div>
-  
-  
-  <style lang="scss">
-    .footer {
-      width: 100%;
-      background: var(--darkBlue);
-      color: var(--white);
-      line-height: 1.6em;
-      padding: 2.5rem var(--margin) 2rem;
-      
-      @media (min-width: 200px) {
-        display: grid;
-        grid-template-columns: 1fr 1fr 2fr;
-        align-items: stretch;
-      }
-      
-      @media (min-width: 200px) {
-        font-size: 0.8rem;
-      }
-  
-      h2 {
-        color: inherit;
-        font-size: 0.7rem;
-        margin: 0 0 var(--quarterNote);
-        width: max-content;
-      }
-  
-      a {
-        color: inherit;
-        text-decoration-color: var(--yellow);
-      }
-  
-      &-wrapper {
-        overflow: hidden;
-        max-width: 100vw;
-      }
-  
-      &__links {
-        padding: 0 var(--quarterNote) var(--quarterNote);
-        margin: 2rem 2rem 2rem 1rem;
-        border-bottom: 1px solid var(--lightBlue);
-  
-        ::marker {
-          color: var(--lightBlue);
-        }
-  
-        @media (min-width: 200px) {
-          border-bottom: 0;
-          border-right: 1px solid var(--lightBlue);
-          padding: 1rem;
-          margin-left: 0;
-        }
-  
-        ul {
-          list-style-type: square;
-          margin: var(--quarterNote) 0;
-        }
-  
-        li {
-          margin: 0.5rem 0 0;
-          font-family: var(--headingFont);
-        }
-      }
-  
-      .fine-print {
-        font-style: italic;
-        margin-left: 1rem;
-        align-self: center;
-  
-        @media (min-width: 200px) {
-         margin-left: 0;
-        }
-  
-        p {
-          margin: .5rem 0;
-          max-width: 44em;
-        }
-      }
-    }
-  </style>
+	import Grid from './Grid.svelte';
+	import { CodepenIcon } from '@indaco/svelte-iconoir';
+  import { GitHubIcon } from '@indaco/svelte-iconoir';
+	import { ListIcon } from '@indaco/svelte-iconoir/icons/ListIcon';
+
+	const currentYear: number = new Date().getFullYear();
+</script>
+
+<div class="footer-wrapper">
+	<footer class="footer">
+		<a data-sveltekit-preload-data="tap" href="https://codepen.io/skillfulman2">
+			<CodepenIcon class="roundedColor" />
+		</a>
+    <a data-sveltekit-preload-data="tap" href="https://github.com/skillfulman2">
+			<GitHubIcon class="roundedColor" />
+		</a>
+	</footer>
+</div>
+
+<style lang="scss">
+</style>
